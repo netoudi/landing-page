85d9b2fdb4b40486c6eb7c21c2969a01f0656ea0
netoudi <netoudi@rocketmail.com>
2021-01-23 12:21:51 -0300

    Add data to SectionModules

--- src/types/api.ts
+++ src/types/api.ts
@@ -43,10 +43,22 @@ export type SectionConceptsProps = {
   }>
 }
 
+type Module = {
+  title: string
+  subtitle: string
+  description: string
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: Module[]
+}
+
 export type LandingPageProps = {
   logo: LogoProps
   header: HeaderProps
   sectionAboutProject: SectionAboutProjectProps
   sectionTech: SectionTechProps
   sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
 }
